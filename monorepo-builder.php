e188a87a27e193082a0e39171db43e69a115f748
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2023-01-11 14:20:34 +0400

    UPDATED MONOREPO REMOVED ECHO WHAT AN IDIOT (talking about myself so its ok)

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -7,7 +7,6 @@ use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $mbConfig): void {
-    echo __DIR__;
     // where are the packages located?
     $mbConfig->packageDirectories([
         // default value
